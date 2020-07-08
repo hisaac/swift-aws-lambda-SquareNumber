348a26682a3b322655f74512e0a4a76d607c93f0
Isaac Halvorson <hello@hisaac.net>
2020-07-07 20:01:19 -0500

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+    name: "SquareNumber",
+    platforms: [.macOS(.v10_13)],
+    products: [
+        .executable(name: "SquareNumber", targets: ["SquareNumber"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git")
+    ],
+    targets: [
+        .target(
+            name: "SquareNumber",
+            dependencies: [
+                .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime")
+            ]
+        )
+    ]
+)
---
+++ Sources/SquareNumber/main.swift
@@ -0,0 +1,14 @@
+import AWSLambdaRuntime
+import AWSLambdaEvents
+
+struct Input: Codable {
+    let number: Double
+}
+
+struct Output: Codable {
+    let result: Double
+}
+
+Lambda.run { (context, input: Input, callback: @escaping (Result<Output, Error>) -> Void) in
+    callback(.success(Output(result: input.number * input.number)))
+}
