a6ebbc452f071a9e7fd06793c53fbd87beca193b
Isaac Halvorson <hello@hisaac.net>
2020-07-07 20:24:56 -0500

    Update build package script modeled after Apple provided one

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # SquareNumber
 
-A description of this package.
+## To Package for Deployment
+
+Run the following docker script in your console from the root directory of the project:
+
+```shell
+docker run --rm --volume "$(pwd)/:/src" --workdir "/src/" swift-lambda-builder scripts/package.sh SquareNumber
+```
+
+Note: If using fish, replace `$(pwd)` with `$PWD`
--- scripts/package.sh
+++ scripts/package.sh
@@ -4,19 +4,12 @@ set -eu
 
 executable=$1
 
-target=.build/lambda/$executable
+target=".build/lambda/$executable"
 rm -rf "$target"
 mkdir -p "$target"
 cp ".build/release/$executable" "$target/"
-cp -Pv \
-	/usr/lib/swift/linux/libBlocksRuntime.so \
-	/usr/lib/swift/linux/libFoundation*.so \
-	/usr/lib/swift/linux/libdispatch.so \
-	/usr/lib/swift/linux/libicu* \
-	/usr/lib/swift/linux/libswiftCore.so \
-	/usr/lib/swift/linux/libswiftDispatch.so \
-	/usr/lib/swift/linux/libswiftGlibc.so \
-	"$target"
+# Add the target dependencies based on ldd
+ldd ".build/release/$executable" | grep swift | awk '{print $3}' | xargs cp -Lv -t "$target"
 cd "$target"
 ln -s "$executable" "bootstrap"
 zip --symlinks lambda.zip *
